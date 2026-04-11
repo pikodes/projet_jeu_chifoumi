04701f2271880f74683963687c93559dd77bb8b7
zahara konate <zarhakonate053@icloud.com>
2026-04-11 14:06:48 +0200

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def joueur_match(y):
    """verifie si le nbre de personne correspond et complète si insufissant"""
    return nbre_joueurs
 
-def jeux(a,b,p):
-   """s'occupe des affrontements entre joueurs"""
+def joueur_machine():
+   pass
 
 def generation_poule(nbre_joueurs):
    """creation de phase de poule"""
@@ -15,27 +15,35 @@ def generation_poule(nbre_joueurs):
 def creer_tournoi():
    pass
 
-def classement_joueur():
-   """charger du ranking des participants tout au long du parcours"""
-   return vainqueur, perdant
+def jeux(a,b,p):
+   """s'occupe des affrontements entre joueurs"""
 
 def calcul_point(x,y,z):
    """"calcul des points phases de poules""" 
-   
    return score_joueur
+
+def classement_joueur():
+   """charger du ranking des participants tout au long du parcours"""
+   return vainqueur, perdant
 
 def poduim(m,n,p):
    """"charger du classement des joueurs"""
    return 
 
-def joueur_machine():
-   pass
-
 def affichage():
    pass
 
-def saisi():
-   pass   
+def saisie(x, y): 
+       """saisie nombre de participant et personne dans le jeu avec gestion d'erreur """
+       try:    
+           x = int(input("entrez nombre de participant"))
+           y = int(input("entrez nombre de joueur"))
+       except ValueError:
+          print('entrez nombre valide')
+       except TabError:
+          print('')
+       
+ 
 
 def main():
    pass
