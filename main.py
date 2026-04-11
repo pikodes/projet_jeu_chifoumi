2930bdd635e97ff4a0b37b98bc063581db7572ba
pikodes <valdestakou@gmail.com>
2026-04-11 11:37:27 +0100

    creation fonction main et autre fonction

---
+++ main.py
@@ -0,0 +1,48 @@
+def nbre_participants(x):
+   """vérifier si le nbre de participants correspond à celui autorisée"""
+
+def joueur_match(y):
+   """verifie si le nbre de personne correspond et complète si insufissant"""
+   return nbre_joueurs
+
+def jeux(a,b,p):
+   """s'occupe des affrontements entre joueurs"""
+
+def generation_poule(nbre_joueurs):
+   """creation de phase de poule"""
+   return poule
+
+def creer_tournoi():
+   pass
+
+def classement_joueur():
+   """charger du ranking des participants tout au long du parcours"""
+   return vainqueur, perdant
+
+def calcul_point(x,y,z):
+   """"calcul des points phases de poules""" 
+   
+   return score_joueur
+
+def poduim(m,n,p):
+   """"charger du classement des joueurs"""
+   return 
+
+def joueur_machine():
+   pass
+
+def affichage():
+   pass
+
+def saisi():
+   pass   
+
+def main():
+   pass
+
+
+
+
+
+
+
